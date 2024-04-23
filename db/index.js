806907fad6be14cefe9c6206dd247c8fe55ee612
Anusha Salman <anushasalman@Anushas-MacBook-Pro.local>
2024-04-23 15:30:58 -0500

    used prisma for posts

--- db/index.js
+++ db/index.js
@@ -8,57 +8,53 @@ const client = new Client(
 );
 
 const getUserById = async (id) => {
-  try{
-const {rows: [user]} = await client.query(`
+  try {
+    const { rows: [user] } = await client.query(`
   SELECT * FROM users
   WHERE id=$1;
 `, [id])
-return user;
-  } 
-  catch(err){
+    return user;
+  }
+  catch (err) {
     throw err;
-}
+  }
 }
 
 const createUser = async (username, password) => {
-  try{
-  const user = await prisma.users.create({
-    data: {
-      username,
-      password
-    }
-  })
+  try {
+    const user = await prisma.users.create({
+      data: {
+        username,
+        password
+      }
+    })
 
-  return user;
-  } catch(err) {
-  throw err;
-}
+    return user;
+  } catch (err) {
+    throw err;
+  }
 };
 
 const createPost = async ({ title, content, ownerId }) => {
   try{
-  const { 
-    rows: [post],
-  } = await client.query(
-    `
-  INSERT INTO posts(title, content, ownerId)
-  VALUES ($1, $2, $3)
-  RETURNING *;
-  `,
-[title, content, ownerId]
-);
-
-return post;
-  } catch(err){
-    throw err;
-  }
+  const post = await prisma.posts.create({
+    data: {
+      title,
+      content,
+      ownerId
+    }
+  })
+  return post;
+} catch(err) {
+  throw err;
+}
 };
 
 const getAllPosts = async () => {
   try{
     const {rows} = await client.query(`
-    SELECT * FROM posts;
-    `);
+  SELECT * FROM posts;
+  `);
 
     return rows;
   } 
@@ -72,9 +68,9 @@ const deletePost = async (postId) => {
     const {rows: [post]} = await client.query(
       `
     DELETE FROM posts
-    WHERE id=${postId}
-    RETURNING *;
-    `
+    WHERE id = ${ postId }
+  RETURNING *;
+  `
   )
   return post;
   } catch(err) {
@@ -87,8 +83,8 @@ const deletePost = async (postId) => {
 const getAllUsersPosts = async (userId) => {
   try{
     const {rows} = await client.query(`
-    SELECT * FROM posts WHERE ownerId=${userId};
-    `);
+  SELECT * FROM posts WHERE ownerId = ${ userId };
+  `);
 
     return rows;
   } 
--- db/seed.js
+++ db/seed.js
@@ -3,47 +3,65 @@ require('dotenv').config();
 const { createUser, createPost } = require("./index");
 const bcrypt = require("bcrypt");
 
+const users = [];
 
 const createUsers = async () => {
-  try{
+  try {
     console.log("Starting to create users...");
     const anusha = await createUser("Anusha", await bcrypt.hash("Salman", 10));
     const sabeen = await createUser("Sabeen", await bcrypt.hash("Abbasi", 10));
     const noor = await createUser("Noor", await bcrypt.hash("abc", 10));
 
-  }catch(err){
+    users.push(anusha);
+    users.push(sabeen);
+    users.push(noor);
+
+    console.log("finished creating users");
+
+  } catch (err) {
     console.log("Error creating users");
     throw err;
   }
-console.log("finished creating users");
 }
 
 const createPosts = async () => {
-try {
-  console.log("starting to create posts");
-  const newYork = await createPost({title: "New York", content: "Best thin crust ever"});
-  const chicago = await createPost({title: "Chicago", content: "Best deep dish ever"});
-  const california = await createPost({title: "California", content: "Best beaches ever"});
-
-}catch(err){
-  console.log("Error creating posts");
-  throw err;
-}
-console.log("finished creating posts");
+  console.log("Starting to create users...");
+  try {
+    // console.log("starting to create posts");
+    const newYork = await createPost({
+      title: "New York",
+      content: "Best thin crust ever",
+      ownerId: users[0].id
+    });
+    const chicago = await createPost({
+      title: "Chicago", 
+      content: "Best deep dish ever",
+      ownerId: users[1].id
+    });
+    const california = await createPost({ 
+      title: "California", 
+      content: "Best beaches ever", 
+      ownerId: users[2].id
+    });
+    console.log("finished creating posts");
+  } catch (err) {
+    console.log("Error creating posts");
+    throw err;
+  }
 }
 
 const rebuildDB = async () => {
-  try{
+  try {
 
-    
+
     await createUsers();
+    await createPosts();
 
 
-
-  }catch(err){
+  } catch (err) {
     console.log("Error during rebuildDB");
     throw err;
   }
-  };
+};
 
-  rebuildDB();
+rebuildDB();
